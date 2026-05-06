ad8bf5856d23884a455394c4ee894cf5c2e34ccc
Benoit Bourgeon <benoit.bourgeon.ext@primetals.com>
2026-05-06 16:31:33 +0200

    update

--- gobenoit-api/src/app.module.ts
+++ gobenoit-api/src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MinerController } from './miner/miner.controller';
@@ -6,7 +9,15 @@ import { BlockchainService } from './blockchain/blockchain.service';
 import { TokenController } from './token/token.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRoot(
+      'mongodb+srv://benoitbourgeoninfo_db_user:1gGuhtzuaStxhJ81@cluster0.cobkrjc.mongodb.net/gobenoit?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController, MinerController, TokenController],
   providers: [AppService, BlockchainService],
 })
